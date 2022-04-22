28a3ae17e24e55ed8ae8a486d49e4448b2d2c555
selena <selena8628@gmail.com>
2022-04-22 10:46:05 +0800

    add animal_config.py

--- animal_config.py
+++ animal_config.py
@@ -1,4 +1,50 @@
 
 class Animal:
     # Selena will implemet here
-    pass
+    def __init__(self, id, sex, weight, height, breed):
+        self.id = id
+        self.sex = sex
+        self.weight = weight
+        self.height = height
+        self.breed = breed
+
+    def eat(self):
+        print('please give me some noodle')
+
+    def sleep(self):
+        print('i am extremely tired')
+
+    def cry(self):
+        print (f'I become{self.weight} ,so fat')
+        
+    def pee(self):
+        print('I drink too much water')
+
+
+class monkey(Animal):
+    def __init__(self, id, sex, weight, height, breed, flur_color):
+        super(). __init__(id, sex, weight, height, breed)
+        self.flur_color = flur_color
+    
+    def climb(self):
+        print(f'i am  {self.flur_color} type , so i climb higher ')
+
+    def sleep(self):
+        print('is time to take a rest')
+
+
+class bear(monkey):
+    def __init__(self, id, sex, weight, height, breed, flur_color, habitat):
+        super(). __init__(id, sex, weight, height, breed, flur_color)
+        self.habitat = habitat
+
+    def howl(self):
+        print(f'this is my {self.habitat} ,leave me alone')
+
+    def sleep (self):
+        print('please be quiet')
+
+
+class tiger(Animal):
+    def sleep(self):
+        print('Go away, i want to sleep now!')
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+from animal_config import bear, monkey, tiger
 from car_config import Bus, SchoolBus, Tank
 
 
 def main():
-    show_how_car_works()
+    #show_how_car_works()
+    show_how_animal_works()
 
+# def show_how_car_works():
+#     my_bus = Bus(1, 'yellow', 3000, 'toyota', 30)
+#     my_schoolBus = SchoolBus(2, 'blue', 1500, 'honda')
+#     my_tank = Tank(3, 'green', 5000, '???')
 
-def show_how_car_works():
-    my_bus = Bus(1, 'yellow', 3000, 'toyota', 30)
-    my_schoolBus = SchoolBus(2, 'blue', 1500, 'honda')
-    my_tank = Tank(3, 'green', 5000, '???')
-
-    car_list = [my_bus, my_schoolBus, my_tank] # List[Car]
-    for each_car in car_list:
-        print('------------------------------')
-        each_car.move()
-        each_car.stop()
+#     car_list = [my_bus, my_schoolBus, my_tank] # List[Car]
+#     for each_car in car_list:
+#         print('------------------------------')
+#         each_car.move()
+#         each_car.stop()
         # if type(each_car) == Bus:
         #     each_car.route()
         #     each_car.pay()
@@ -23,8 +24,23 @@ def show_how_car_works():
 
 def show_how_animal_works():
     # Selena will implemet here
-    pass
+    monkey1 = monkey(1, 'male', 35, 125, 'mammal', 'brown')
+    monkey2 = monkey(2, 'female', 75, 105, 'mammal', 'black')
+    bear1 = bear(3, 'male', 180, 200, 'mammal', 'white', 'ice')
+    bear2 = bear(4, 'male', 230, 300, 'mammal', 'brown', 'forest')
+    tiger1 = tiger(5, 'female', 100, 80, 'mammal')
+    tiger2 = tiger(6, 'male', 150, 90, 'mammal')
 
-
+    animal_list = [monkey1, monkey2, bear1, bear2, tiger1, tiger2]
+    for animal in animal_list:
+        print(f"\n{animal.id}")
+        print('-----------------------')
+        animal.sleep()
+        print('-----------------------')
+        animal.cry()
+        print('-----------------------')
+        animal.pee()
+       
+    
 if __name__ == '__main__':
     main()
